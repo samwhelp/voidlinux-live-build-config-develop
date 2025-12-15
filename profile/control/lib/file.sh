2af0eeb37a8ce46998358334f4122951810c7ae7
samwhelp <samwhelp@users.noreply.github.com>
2025-12-15 21:08:04 +0000

    fix

--- profile/control/lib/file.sh
+++ profile/control/lib/file.sh
@@ -5,9 +5,29 @@ util_dir_copy () {
 	local des_dir_path="${2}"
 
 	mkdir -p "${src_dir_path}"
-
 	mkdir -p "${des_dir_path}"
 
 	cp -rfT "${src_dir_path}" "${des_dir_path}"
+
+}
+
+
+util_file_copy () {
+
+	local src_file_path="${1}"
+	local des_file_path="${2}"
+
+	local src_dir_path="$(dirname "${src_file_path}")"
+	local des_dir_path="$(dirname "${des_file_path}")"
+
+	mkdir -p "${src_dir_path}"
+	mkdir -p "${des_dir_path}"
+
+	if ! [ -e "${src_file_path}" ]; then
+
+		return 0
+	if
+
+	cp -f "${src_file_path}" "${des_file_path}"
 
 }
