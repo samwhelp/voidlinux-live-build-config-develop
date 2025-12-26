b2dd9cce53fb94000d59f3dd90aa4526d03d70e2
samwhelp <samwhelp@users.noreply.github.com>
2025-12-27 07:23:01 +0800

    fix

--- profile/control/lib/core.sh
+++ profile/control/lib/core.sh
@@ -5,7 +5,14 @@ core_args_load () {
 	## trim newline version
 	##
 
+
 	local the_args_file="${1}"
+
+	if ! [ -e "${the_args_file}" ]; then
+		return
+	fi
+
+
 	local the_args=$(cat "${the_args_file}")
 
 	echo ${the_args}
@@ -19,7 +26,14 @@ core_list_load () {
 	## not trim newline version
 	##
 
+
 	local the_list_file="${1}"
+
+	if ! [ -e "${the_args_file}" ]; then
+		return
+	fi
+
+
 	local the_list="$(cat "${the_list_file}")"
 
 	echo "${the_list}"
